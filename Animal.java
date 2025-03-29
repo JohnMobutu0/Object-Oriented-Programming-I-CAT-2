cf51c8e6befa7b926f70e05a1e235f171cbeda1a
Mobutu <mulumeoderhwa.cirhuza@strathmore.edu>
2025-03-29 13:50:48 +0300

    first commit

---
+++ Animal.java
@@ -0,0 +1,5 @@
+public class Animal {
+    public void makeSound() {
+        System.out.println("An animal makes a sound.");
+    }
+}
---
+++ MathOperations.java
@@ -0,0 +1,11 @@
+public class MathOperations {
+    // Overloaded method: multiplies two integers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Overloaded method: multiplies three integers
+    public int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
+}
